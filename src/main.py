e73009d1e5027769dda648531a9d12e713657cb1
Nnelsan <nelsondsp20@gmail.com>
2026-06-11 12:41:28 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from auth import fazer_login, fazer_logout
+from quartos import adicionar_quarto, listar_quartos
 
 utilizador = fazer_login()
 
 if utilizador:
+    adicionar_quarto()
+    listar_quartos()
     fazer_logout(utilizador)
